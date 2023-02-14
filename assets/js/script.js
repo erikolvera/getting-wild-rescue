ef5a9e39f57dca5c15b9e187f2760916d08de9ce
KadenDean7 <kadenlukasdean@hotmail.com>
2023-02-13 23:48:46 -0600

    added carousel with placeholder imgs

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ const secret = 'hI7Kyt4lVjtoOIywuub9YEMIzvaylnoscXD3BUzb';
 
 let token = "";
 
+//dog api keys
+
+const url = "https://api.thedogapi.com/v1/images/search?limit=10";
+const api_key = "live_tixKAnEqLJ2vdEof1D0C20MT6WhTiOxVaDKJ1buCcBaDJQzYgvNLYGVsaFHs25zT";
+
 //obtain token for the PetFinder API
 getToken();
 
@@ -70,4 +75,45 @@ $("#submit").click(function() {
     console.log(userInputEL.value)
     findPetsNearby(userInputEL.value);
     return false;
+})
+
+function setImages(){
+    fetch(url,{headers:{
+        "live_tixKAnEqLJ2vdEof1D0C20MT6WhTiOxVaDKJ1buCcBaDJQzYgvNLYGVsaFHs25zT": api_key
+        }})
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => $(".img").attr("src", data[0].url))
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+setImages();
